26d9633cf6ab0d357d0b117d7a5bd0d4afc2516d
Raviteja <tejakommineni798@gmail.com>
2026-01-21 20:47:40 +0530

    Modified firstprogram --Addition operation

--- firstprogram.java
+++ firstprogram.java
@@ -2,6 +2,9 @@ class Demo
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello Java!!!");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
     }
 }
